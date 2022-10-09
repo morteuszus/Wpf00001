1061af3d853fb4b3feb872c9abd190f48394e055
Admin <Admin@DESKTOP-8CO37JH>
2022-10-09 19:42:12 +0200

    Dodaj pliki projektów.

---
+++ Wpf00001/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf00001.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged()
+    }
+}
