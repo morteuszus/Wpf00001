11e005127081672ec24788bd3b93cbaacf7a4e1f
Filip Kaczmarek <ponton007@onet.pl>
2022-10-10 21:29:41 +0200

    kluczowe zmiany - Theme

--- Wpf00001/Core/RelayCommand.cs
+++ Wpf00001/Core/RelayCommand.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Wpf00001.Core
 {
     class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object,bool> canExecute;
+        private Action<object> _execute;
+        private Func<object,bool> _canExecute;
 
         public event EventHandler CanExecuteChanged
         {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
 
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute==null||_canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
         }
     }
 }
